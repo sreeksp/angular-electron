fb6b5c3cb0911883d64f68e8a7b7bd1590f9f02c
sreeksp <sreekanthpalaparty30@gmail.com>
2020-04-06 23:07:44 -0700

    Update main.ts

--- electron/main.ts
+++ electron/main.ts
@@ -6,11 +6,10 @@ import * as fs from 'fs';
 let win: BrowserWindow;
 app.on('ready', createWindow);
 
-// const path = require('path');
 
-require('electron-reload')(__dirname, {
-  electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
-});
+// require('electron-reload')(__dirname, {
+//   electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+// });
 app.on('activate', () => {
   if (win === null) {
     createWindow();
@@ -24,14 +23,14 @@ function createWindow() {
     webPreferences: { nodeIntegration: true }
   });
 
-  // win.loadURL(
-  //   url.format({
-  //     pathname: path.join(__dirname, `/../../dist/angular-electron/index.html`),
-  //     protocol: 'file:',
-  //     slashes: true
-  //   })
-  // );
-  win.loadURL('http://localhost:4200/');
+  win.loadURL(
+    url.format({
+      pathname: path.join(__dirname, `/../../dist/angular-electron/index.html`),
+      protocol: 'file:',
+      slashes: true
+    })
+  );
+ // win.loadURL('http://localhost:4200/');
   win.webContents.openDevTools();
 
   win.on('closed', () => {
